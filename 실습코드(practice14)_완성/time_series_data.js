3d8d5cf9777cfc5baf7821965758b75450942fee
specialscene <kimsingogo@gmail.com>
2018-01-08 16:21:28 +0900

    D3_with_SDH

---
+++ 실습코드(practice14)_완성/time_series_data.js
@@ -0,0 +1,618 @@
+var data =
+[
+  {
+    "date": "Jan-00",
+    "price": 1394.46,
+    "cost": 563.85
+  },
+  {
+    "date": "Feb-00",
+    "price": 1366.42,
+    "cost": 426.55
+  },
+  {
+    "date": "Mar-00",
+    "price": 1498.58,
+    "cost": 572.67
+  },
+  {
+    "date": "Apr-00",
+    "price": 1452.43,
+    "cost": 405.45
+  },
+  {
+    "date": "May-00",
+    "price": 1420.6,
+    "cost": 506.17
+  },
+  {
+    "date": "Jun-00",
+    "price": 1454.6,
+    "cost": 470.19
+  },
+  {
+    "date": "Jul-00",
+    "price": 1430.83,
+    "cost": 478.83
+  },
+  {
+    "date": "Aug-00",
+    "price": 1517.68,
+    "cost": 436.5
+  },
+  {
+    "date": "Sep-00",
+    "price": 1436.51,
+    "cost": 485.78
+  },
+  {
+    "date": "Oct-00",
+    "price": 1429.4,
+    "cost": 535.76
+  },
+  {
+    "date": "Nov-00",
+    "price": 1314.95,
+    "cost": 511.93
+  },
+  {
+    "date": "Dec-00",
+    "price": 1320.28,
+    "cost": 476.21
+  },
+  {
+    "date": "Jan-01",
+    "price": 1366.01,
+    "cost": 423.65
+  },
+  {
+    "date": "Feb-01",
+    "price": 1239.94,
+    "cost": 452.02
+  },
+  {
+    "date": "Mar-01",
+    "price": 1160.33,
+    "cost": 509.18
+  },
+  {
+    "date": "Apr-01",
+    "price": 1249.46,
+    "cost": 473.88
+  },
+  {
+    "date": "May-01",
+    "price": 1255.82,
+    "cost": 586.41
+  },
+  {
+    "date": "Jun-01",
+    "price": 1224.38,
+    "cost": 498.08
+  },
+  {
+    "date": "Jul-01",
+    "price": 1211.23,
+    "cost": 502.57
+  },
+  {
+    "date": "Aug-01",
+    "price": 1133.58,
+    "cost": 526.26
+  },
+  {
+    "date": "Sep-01",
+    "price": 1040.94,
+    "cost": 575.18
+  },
+  {
+    "date": "Oct-01",
+    "price": 1059.78,
+    "cost": 508.76
+  },
+  {
+    "date": "Nov-01",
+    "price": 1139.45,
+    "cost": 499.3
+  },
+  {
+    "date": "Dec-01",
+    "price": 1148.08,
+    "cost": 468.86
+  },
+  {
+    "date": "Jan-02",
+    "price": 1130.2,
+    "cost": 422.76
+  },
+  {
+    "date": "Feb-02",
+    "price": 1106.73,
+    "cost": 595.84
+  },
+  {
+    "date": "Mar-02",
+    "price": 1147.39,
+    "cost": 491.71
+  },
+  {
+    "date": "Apr-02",
+    "price": 1076.92,
+    "cost": 457.89
+  },
+  {
+    "date": "May-02",
+    "price": 1067.14,
+    "cost": 549.04
+  },
+  {
+    "date": "Jun-02",
+    "price": 989.82,
+    "cost": 593.23
+  },
+  {
+    "date": "Jul-02",
+    "price": 911.62,
+    "cost": 439.23
+  },
+  {
+    "date": "Aug-02",
+    "price": 916.07,
+    "cost": 562.45
+  },
+  {
+    "date": "Sep-02",
+    "price": 815.28,
+    "cost": 543.43
+  },
+  {
+    "date": "Oct-02",
+    "price": 885.76,
+    "cost": 519.09
+  },
+  {
+    "date": "Nov-02",
+    "price": 936.31,
+    "cost": 484.4
+  },
+  {
+    "date": "Dec-02",
+    "price": 879.82,
+    "cost": 469.02
+  },
+  {
+    "date": "Jan-03",
+    "price": 855.7,
+    "cost": 401.83
+  },
+  {
+    "date": "Feb-03",
+    "price": 841.15,
+    "cost": 591.94
+  },
+  {
+    "date": "Mar-03",
+    "price": 848.18,
+    "cost": 403.73
+  },
+  {
+    "date": "Apr-03",
+    "price": 916.92,
+    "cost": 537.86
+  },
+  {
+    "date": "May-03",
+    "price": 963.59,
+    "cost": 453.25
+  },
+  {
+    "date": "Jun-03",
+    "price": 974.5,
+    "cost": 490.45
+  },
+  {
+    "date": "Jul-03",
+    "price": 990.31,
+    "cost": 443.45
+  },
+  {
+    "date": "Aug-03",
+    "price": 1008.01,
+    "cost": 503.5
+  },
+  {
+    "date": "Sep-03",
+    "price": 995.97,
+    "cost": 540.7
+  },
+  {
+    "date": "Oct-03",
+    "price": 1050.71,
+    "cost": 595
+  },
+  {
+    "date": "Nov-03",
+    "price": 1058.2,
+    "cost": 591.82
+  },
+  {
+    "date": "Dec-03",
+    "price": 1111.92,
+    "cost": 437.46
+  },
+  {
+    "date": "Jan-04",
+    "price": 1131.13,
+    "cost": 474.75
+  },
+  {
+    "date": "Feb-04",
+    "price": 1144.94,
+    "cost": 403.3
+  },
+  {
+    "date": "Mar-04",
+    "price": 1126.21,
+    "cost": 514.48
+  },
+  {
+    "date": "Apr-04",
+    "price": 1107.3,
+    "cost": 467.08
+  },
+  {
+    "date": "May-04",
+    "price": 1120.68,
+    "cost": 442.41
+  },
+  {
+    "date": "Jun-04",
+    "price": 1140.84,
+    "cost": 589.99
+  },
+  {
+    "date": "Jul-04",
+    "price": 1101.72,
+    "cost": 410.81
+  },
+  {
+    "date": "Aug-04",
+    "price": 1104.24,
+    "cost": 512.25
+  },
+  {
+    "date": "Sep-04",
+    "price": 1114.58,
+    "cost": 510.88
+  },
+  {
+    "date": "Oct-04",
+    "price": 1130.2,
+    "cost": 415.3
+  },
+  {
+    "date": "Nov-04",
+    "price": 1173.82,
+    "cost": 549.71
+  },
+  {
+    "date": "Dec-04",
+    "price": 1211.92,
+    "cost": 460.57
+  },
+  {
+    "date": "Jan-05",
+    "price": 1181.27,
+    "cost": 498.54
+  },
+  {
+    "date": "Feb-05",
+    "price": 1203.6,
+    "cost": 588.27
+  },
+  {
+    "date": "Mar-05",
+    "price": 1180.59,
+    "cost": 571.99
+  },
+  {
+    "date": "Apr-05",
+    "price": 1156.85,
+    "cost": 584.54
+  },
+  {
+    "date": "May-05",
+    "price": 1191.5,
+    "cost": 562.33
+  },
+  {
+    "date": "Jun-05",
+    "price": 1191.33,
+    "cost": 563.64
+  },
+  {
+    "date": "Jul-05",
+    "price": 1234.18,
+    "cost": 443.84
+  },
+  {
+    "date": "Aug-05",
+    "price": 1220.33,
+    "cost": 559.14
+  },
+  {
+    "date": "Sep-05",
+    "price": 1228.81,
+    "cost": 578.75
+  },
+  {
+    "date": "Oct-05",
+    "price": 1207.01,
+    "cost": 571.91
+  },
+  {
+    "date": "Nov-05",
+    "price": 1249.48,
+    "cost": 551.28
+  },
+  {
+    "date": "Dec-05",
+    "price": 1248.29,
+    "cost": 587.72
+  },
+  {
+    "date": "Jan-06",
+    "price": 1280.08,
+    "cost": 532.5
+  },
+  {
+    "date": "Feb-06",
+    "price": 1280.66,
+    "cost": 574.67
+  },
+  {
+    "date": "Mar-06",
+    "price": 1294.87,
+    "cost": 565.45
+  },
+  {
+    "date": "Apr-06",
+    "price": 1310.61,
+    "cost": 543.77
+  },
+  {
+    "date": "May-06",
+    "price": 1270.09,
+    "cost": 457.2
+  },
+  {
+    "date": "Jun-06",
+    "price": 1270.2,
+    "cost": 520.64
+  },
+  {
+    "date": "Jul-06",
+    "price": 1276.66,
+    "cost": 417.31
+  },
+  {
+    "date": "Aug-06",
+    "price": 1303.82,
+    "cost": 438.55
+  },
+  {
+    "date": "Sep-06",
+    "price": 1335.85,
+    "cost": 509.69
+  },
+  {
+    "date": "Oct-06",
+    "price": 1377.94,
+    "cost": 507.82
+  },
+  {
+    "date": "Nov-06",
+    "price": 1400.63,
+    "cost": 505.03
+  },
+  {
+    "date": "Dec-06",
+    "price": 1418.3,
+    "cost": 575.06
+  },
+  {
+    "date": "Jan-07",
+    "price": 1438.24,
+    "cost": 435.23
+  },
+  {
+    "date": "Feb-07",
+    "price": 1406.82,
+    "cost": 483.02
+  },
+  {
+    "date": "Mar-07",
+    "price": 1420.86,
+    "cost": 465.67
+  },
+  {
+    "date": "Apr-07",
+    "price": 1482.37,
+    "cost": 407.87
+  },
+  {
+    "date": "May-07",
+    "price": 1530.62,
+    "cost": 593.85
+  },
+  {
+    "date": "Jun-07",
+    "price": 1503.35,
+    "cost": 449.83
+  },
+  {
+    "date": "Jul-07",
+    "price": 1455.27,
+    "cost": 528.64
+  },
+  {
+    "date": "Aug-07",
+    "price": 1473.99,
+    "cost": 432.41
+  },
+  {
+    "date": "Sep-07",
+    "price": 1526.75,
+    "cost": 429.61
+  },
+  {
+    "date": "Oct-07",
+    "price": 1549.38,
+    "cost": 497.97
+  },
+  {
+    "date": "Nov-07",
+    "price": 1481.14,
+    "cost": 587.19
+  },
+  {
+    "date": "Dec-07",
+    "price": 1468.36,
+    "cost": 401.13
+  },
+  {
+    "date": "Jan-08",
+    "price": 1378.55,
+    "cost": 468.3
+  },
+  {
+    "date": "Feb-08",
+    "price": 1330.63,
+    "cost": 511.03
+  },
+  {
+    "date": "Mar-08",
+    "price": 1322.7,
+    "cost": 593.18
+  },
+  {
+    "date": "Apr-08",
+    "price": 1385.59,
+    "cost": 542.03
+  },
+  {
+    "date": "May-08",
+    "price": 1400.38,
+    "cost": 490.99
+  },
+  {
+    "date": "Jun-08",
+    "price": 1280,
+    "cost": 577.78
+  },
+  {
+    "date": "Jul-08",
+    "price": 1267.38,
+    "cost": 522.03
+  },
+  {
+    "date": "Aug-08",
+    "price": 1282.83,
+    "cost": 494.5
+  },
+  {
+    "date": "Sep-08",
+    "price": 1166.36,
+    "cost": 557.05
+  },
+  {
+    "date": "Oct-08",
+    "price": 968.75,
+    "cost": 424.16
+  },
+  {
+    "date": "Nov-08",
+    "price": 896.24,
+    "cost": 457.62
+  },
+  {
+    "date": "Dec-08",
+    "price": 903.25,
+    "cost": 426.04
+  },
+  {
+    "date": "Jan-09",
+    "price": 825.88,
+    "cost": 401.36
+  },
+  {
+    "date": "Feb-09",
+    "price": 735.09,
+    "cost": 409.76
+  },
+  {
+    "date": "Mar-09",
+    "price": 797.87,
+    "cost": 560.78
+  },
+  {
+    "date": "Apr-09",
+    "price": 872.81,
+    "cost": 424.04
+  },
+  {
+    "date": "May-09",
+    "price": 919.14,
+    "cost": 585.43
+  },
+  {
+    "date": "Jun-09",
+    "price": 919.32,
+    "cost": 574.55
+  },
+  {
+    "date": "Jul-09",
+    "price": 987.48,
+    "cost": 561.1
+  },
+  {
+    "date": "Aug-09",
+    "price": 1020.62,
+    "cost": 422.13
+  },
+  {
+    "date": "Sep-09",
+    "price": 1057.08,
+    "cost": 549.69
+  },
+  {
+    "date": "Oct-09",
+    "price": 1036.19,
+    "cost": 516.29
+  },
+  {
+    "date": "Nov-09",
+    "price": 1095.63,
+    "cost": 437.77
+  },
+  {
+    "date": "Dec-09",
+    "price": 1115.1,
+    "cost": 542.43
+  },
+  {
+    "date": "Jan-10",
+    "price": 1073.87,
+    "cost": 504.91
+  },
+  {
+    "date": "Feb-10",
+    "price": 1104.49,
+    "cost": 421.58
+  },
+  {
+    "date": "Mar-10",
+    "price": 1140.45,
+    "cost": 422.82
+  }
+];
